efa0a3130b7e2a49f3be1e92d803afd19d7b38aa
luiz-borges <jose.iscosistemas@gmail.com>
2024-02-21 09:42:46 -0800

    astro config

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,22 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  {import = "astrocommunity.colorscheme.catppuccin" },
+  {import = "astrocommunity.pack.html-css"},
+  {import = "astrocommunity.pack.json"},
+  {import = "astrocommunity.pack.markdown"},
+  {import = "astrocommunity.pack.prisma"},
+  {import = "astrocommunity.pack.tailwindcss"},
+  {import = "astrocommunity.pack.typescript"},
+  {import = "astrocommunity.pack.typescript-all-in-one"},
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = 120,
+      disabled_filetypes = { "help" },
+    },
+  },
 }
